ac651bf2e7556fd2c4ad7a4ce802991c62518185
Shauna Griffin <shaunahennessygriffin@gmail.com>
2017-06-07 03:23:29 +0000

    Built tiny app that returns tiny link to redirect to the full website

--- express_server.js
+++ express_server.js
@@ -4,14 +4,40 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
-app.set("view engine", "ejs")
+
+function generateRandomString(length, chars) {
+let result = '';
+    for (var i = 0; i < 6; i++) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs")
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/urls/new", (req, res) => {
+
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  var rString = generateRandomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+  urlDatabase[rString] = req.body.longURL;
+  var newString = res.send("Your new tiny string is " + "http://localhost:8080/u/" + rString);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  var shortURL = req.params.shortURL;
+  var longURL = urlDatabase[shortURL];
+
+  res.redirect(longURL);
+
+});
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
@@ -32,12 +58,4 @@ app.get("/urls/:id", (req, res) => {
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
---
+++ extras.js
@@ -0,0 +1,7 @@
+function randomString(length, chars) {
+var result = '';
+    for (var i = 0; i < 6; i++) result += chars[Math.floor(Math.random() * chars.length)];
+    return result;
+}
+var rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+console.log(rString);
